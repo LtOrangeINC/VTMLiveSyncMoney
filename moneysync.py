04c0898ea102cf2c0d60fd90f338fdf3c3c3c356
LtOrangeINC <35194145+LtOrangeINC@users.noreply.github.com>
2021-09-02 20:04:38 +0100

    Update moneysync.py

--- moneysync.py
+++ moneysync.py
@@ -17,7 +17,7 @@ money =  bank.find("td").find_next_sibling("td").text
 numbers = money.translate({ord(i): None for i in "€$£"})
 print("Value to set is:" + numbers)
 value = int(float(numbers))
-my_pid = None
+my_pid = None  #process enumeration to find pid of eurotrucks2.exe
 pids = psutil.pids()
 for pid in pids:
     ps = psutil.Process(pid)
@@ -30,7 +30,7 @@ processHandle = win32api.OpenProcess(PROCESS_ALL_ACCESS, False, my_pid)
 modules = win32process.EnumProcessModules(processHandle)
 processHandle.close()
 base_addr = modules[0] # first item on the list 
-#print(hex(base_addr))
+
 def main():
     base_address = hex(base_addr)  #base address of the exe (find out with pymem)
     u = int(base_address, 0)
@@ -39,15 +39,12 @@ def main():
     offsets = [0x0C, 0x10] #list offsets
    
 
-    rmw = ReadWriteMemory()
+    rmw = ReadWriteMemory() 
     process = rmw.get_process_by_name("eurotrucks2.exe")
     process.open()
     my_pointer = process.get_pointer(pointer_static_address, offsets=offsets)
     pointer_value = process.read(my_pointer)
-    #print(pointer_value)
     process.write(my_pointer, value)
-    #time.sleep(5)
-    #print(pointer_value)
     print("Value set, try opening the bank or progress history, to refresh the value")
     
 main()
